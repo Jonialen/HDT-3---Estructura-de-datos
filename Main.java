83fdfd1994d0dbe0b372df8156f72bc84b10cc6d
Qu3zada22 <139565119+Qu3zada22@users.noreply.github.com>
2024-02-06 21:48:59 -0600

    Clases nuevas

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SortingBenchmark benchmark = new SortingBenchmark();
+        benchmark.runBenchmark();
+    }
+}
